032f6fd36aa923ff0da29e030a601f906c98152b
Twincasper <justinwatson651@gmail.com>
2025-02-28 10:51:46 -0500

    Added glab code

--- src/main/java/showData.java
+++ src/main/java/showData.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 public class showData {
+    public static void main(String[] args) {
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue: mybooklist)
+        {
+            System.out.println(showValue.getNumber() + " " + showValue.getName() + " " + showValue.getCategory() + " " + showValue.getAuthor());
+        }
+    }
 }
